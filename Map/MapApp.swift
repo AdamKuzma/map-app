f53f7642f7c6b3c4973bbcef95460f99b8f684f2
adamkuzma <adam@esecure.cc>
2025-03-20 16:59:48 -0400

    Initial Commit

---
+++ Map/MapApp.swift
@@ -0,0 +1,17 @@
+//
+//  MapApp.swift
+//  Map
+//
+//  Created by Adam Kuzma on 3/20/25.
+//
+
+import SwiftUI
+
+@main
+struct MapApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MapTests/MapTests.swift
@@ -0,0 +1,17 @@
+//
+//  MapTests.swift
+//  MapTests
+//
+//  Created by Adam Kuzma on 3/20/25.
+//
+
+import Testing
+@testable import Map
+
+struct MapTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
